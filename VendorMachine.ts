e9887cb2790d2f069c09f11ceee72c9e58c4efd0
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-07 11:47:25 +0200

    new exercice

--- VendorMachine.ts
+++ VendorMachine.ts
@@ -12,24 +12,31 @@ class VendorMachine {
 
   public buySnack(): void {
     this.isOn = true;
+    if (this.snacksQty <= 0) {
+      throw new Error("bien essayé, mais y'a plus des snacks");
+    }
     this.snacksQty = -1;
     this.money = 2;
   }
 
   public reset(): void {
     this.isOn = false;
+    if (this.snacksQty > 50) {
+      throw new Error("la machine est deja rempli");
+    }
     this.snacksQty = 50;
+
     this.money = 0;
     this.isOn = true;
   }
 
   public shootWithFoot(): void {
     this.snacksQty = -5;
-
     //
     if (this.money < 20) {
       throw new Error("bien essayé, mais y'a plus d'argent");
     }
+
     this.money = -20;
     this.isOn = false;
   }
